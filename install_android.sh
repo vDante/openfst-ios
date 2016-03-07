62eab43e8a5904f00a65e2e7da5aa8521af2e158
Kevin Yang <skyang@gmail.com>
2016-03-07 15:16:52 -0500

    Modify install_android to take parameters for toolchain, and install prefix.

--- install_android.sh
+++ install_android.sh
@@ -1,8 +1,12 @@
 # This assumes you've build the android toolchain, and that you want to build arm.
 # This will install to /opt/arm-tools
 
-export TOOLCHAIN_PREFIX=/tmp/my-android-toolchain/bin/arm-linux-androideabi
+set -x
 
+TOOLCHAIN_PREFIX=${1:-/tmp/my-android-toolchain/bin/arm-linux-androideabi}
+INSTALL_PREFIX=${2:/opt/arm-tools}
+
+echo "Using toolchain prefix " $TOOLCHAIN_PREFIX
 export CPP=${TOOLCHAIN_PREFIX}-cpp
 export AR=${TOOLCHAIN_PREFIX}-ar
 export AS=${TOOLCHAIN_PREFIX}-as
@@ -12,6 +16,6 @@ export CXX=${TOOLCHAIN_PREFIX}-g++
 export LD=${TOOLCHAIN_PREFIX}-ld
 export RANLIB=${TOOLCHAIN_PREFIX}-ranlib
 
-./configure --with-pic --enable-shared --enable-static --host=arm-linux-androideabi --prefix=/opt/arm-tools
+./configure --with-pic --enable-shared --enable-static --host=arm-linux-androideabi --prefix=$INSTALL_PREFIX
 make
 make install
